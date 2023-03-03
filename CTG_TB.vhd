be33196a0ee4c1aca31912f16b1439d93b140770
Gonçalo Paiva Gouveia <68733294+gongouveia@users.noreply.github.com>
2023-03-03 15:04:55 +0000

    Add files via upload

--- CTG_TB.vhd
+++ CTG_TB.vhd
@@ -12,16 +12,17 @@ architecture bench of CTG_tb is
       Port (
           CLK : IN  STD_LOGIC;
           RESET : IN  STD_LOGIC;           
-          NEW_VALUE :IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-          OUT_VALUE : OUT std_logic
+          NEW_VALUE :IN SIGNED(11 DOWNTO 0);
+          OUT_VALUE : OUT STD_LOGIC
+          --STATE_at : out INTEGER
+
            );
   end component;
 
   signal CLK: STD_LOGIC := '0';
   signal RESET: STD_LOGIC := '0';
-  signal NEW_VALUE: STD_LOGIC_VECTOR(11 DOWNTO 0);
-  signal OUT_VALUE: std_logic ;
-
+  signal NEW_VALUE: SIGNED(11 DOWNTO 0);
+  signal OUT_VALUE: STD_LOGIC := '0';
   constant clock_period: time := 10 ns;
 
 
@@ -32,11 +33,13 @@ begin
   uut: CTG port map ( CLK       => CLK,
                       RESET     => RESET,
                       NEW_VALUE => NEW_VALUE,
-                      OUT_VALUE => OUT_VALUE );
+                      OUT_VALUE => OUT_VALUE
+                     );
 
   stimulus: process
   begin
-  
+       RESET <= '0';
+       wait for 3*clock_period;
     -- Put initialisation code here
        RESET <= '1';
        wait for clock_period;
@@ -50,6 +53,8 @@ begin
        wait for clock_period;
        NEW_VALUE <=  "000000000001";
        wait for clock_period;
+      RESET <= '0';
+
        NEW_VALUE <=  "000000000011";
        wait for clock_period;
        NEW_VALUE <=  "000000000111";
@@ -63,7 +68,11 @@ begin
        NEW_VALUE <=  "000000000111";
        wait for clock_period;
        NEW_VALUE <=  "111111111111"; 
- 
+       wait for clock_period;
+       wait for clock_period;
+       wait for clock_period;
+       wait for clock_period;
+
     wait;
   end process;
 
